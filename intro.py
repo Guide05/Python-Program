f17b0ff019571e5148a5a88193de446af4627da1
Guide05 <danielsatombo@gmail.com>
2025-08-11 10:00:52 +0200

    Add files via upload

---
+++ intro.py
@@ -0,0 +1,40 @@
+# """PLP = "Academy" #string"""
+# student_name = "Satombo Guide"
+# student_ID= 1738
+# student_Age= 23.7
+# Fees_clearance= True
+
+# Fees_clearance= True
+# #output
+# print(student_name,student_ID, student_Age,Fees_clearance )
+
+#lists
+Langauges = ["python","java", "c++", 178]
+print(Langauges)
+shopping_list= ["milk", "drink", "juice"]
+print(shopping_list)
+print(Langauges[0])
+
+
+#Tuples
+fruits= ("apple","bananas","guava", 449.67)
+print(fruits)
+
+#Sets
+set_A={1,3,5,8}
+set_B={4,9,0.5,14}
+print(set_A)
+print(set_B)
+
+
+#Dictionary
+student_info={"Name": "Guide Satombo", "ID": 1738}
+print (student_info)
+
+#Logical ops
+a=10
+b=20
+c=30
+print(a<b and b<c)
+print(a>b or b>c)
+print(not (a<b))
